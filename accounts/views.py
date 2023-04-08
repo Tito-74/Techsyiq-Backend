442e62e90a4e96d7cb6a71fb1340564a9d2b6d04
Tito-74 <kipkirui133@gmail.com>
2023-04-08 10:53:09 +0300

    added profile view

--- accounts/views.py
+++ accounts/views.py
@@ -1,4 +1,12 @@
 from django.shortcuts import render
-
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .serializers import UserSerializer
 # Create your views here.
 
+
+@api_view(['GET'])
+def profile(request):
+    user = request.user
+    serialized_user = UserSerializer(user).data
+    return Response({'user':serialized_user})
