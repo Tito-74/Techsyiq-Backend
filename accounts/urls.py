e84b10f580e9184602a94ce94e16ca17a0f5ccb8
Tito-74 <kipkirui133@gmail.com>
2023-04-08 13:06:23 +0300

    added login view

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-from .views import profile
+from .views import profile, login_view
 urlpatterns = [
     path('profile', profile, name= 'profile'),
-    
+    path('login', login_view, name= 'login'),
 ]
