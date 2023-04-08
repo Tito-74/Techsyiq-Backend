675ab7825ae468c985df5c5d73303611efa79108
Tito-74 <kipkirui133@gmail.com>
2023-04-08 17:00:58 +0300

    modify the app

---
+++ Team/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+from django.conf import settings
+from cloudinary.models import CloudinaryField
+
+# Create your models here.
+class TechsyiqTeam(models.Model):
+  name = models.CharField(max_length=255)
+  title= models.CharField(max_length=255)
+  social_media_link = models.URLField(blank=True)
+  description = models.TextField(blank=False)
+  image = CloudinaryField('images', default=None)
+  author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
+  def __str__(self):
+    return self.name
