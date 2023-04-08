eee5eb4c8d2ca4a2c49ef5123775f34e55a0bfdb
Tito-74 <kipkirui133@gmail.com>
2023-04-08 14:19:56 +0300

    added refresh token endpoint

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import profile, login_view
+from .views import profile, login_view,refresh_token_view  
 urlpatterns = [
     path('profile', profile, name= 'profile'),
     path('login', login_view, name= 'login'),
+    path('refresh_token', refresh_token_view, name='refresh_token')
 ]
