5b4844581452ea61c32986b04786591a44e09ef6
Tito-74 <kipkirui133@gmail.com>
2023-04-09 14:00:35 +0300

    added course endpoints and view

---
+++ Course/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from .views import *
+
+urlpattern = [
+    path('add', add_course_details, name='add'),
+    path('fetch_course', get_all_course, name='fetch_course'),
+    path('update/<int:pk>', update_course_details, name='update'),
+    path('delete/<int:pk>', delete_course_details, name='delete'),
+]
--- Course/views.py
+++ Course/views.py
@@ -1,3 +1,59 @@
 from django.shortcuts import render
-
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.response import Response
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.permissions import AllowAny
+from rest_framework import status
+from .serializers import CourseSerializer
+from .models import Course
 # Create your views here.
+
+# add team member
+@api_view(['POST'])
+@csrf_exempt
+def add_course_details(request):
+    serializer = CourseSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+# get all members
+@api_view(['GET'])
+@permission_classes([AllowAny])
+def get_all_course(request):
+    course = Course.objects.all()
+    serializer = CourseSerializer(course, many=True)
+    return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+# update member details
+@api_view(['PUT'])
+@csrf_exempt
+def update_course_details(self, request, *args, **kwargs):
+    pk = kwargs.get('pk')
+    try:
+        course = Course.Objects.filter(id=pk).first()
+    
+    except course.DoesNotExist:
+        return Response({"message":"Member does not exist"}, status=status.HTTP_404_NOT_FOUND)
+    
+    serializer = CourseSerializer(instance=course, data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response({"message":"Member updated successfully"}, status=status.HTTP_200_OK)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['DELETE'])
+def delete_course_details(self, request, *args, **kwargs):
+    pk = kwargs.get('pk')
+    try:
+        course = Course.Objects.filter(id=pk).first()
+    
+    except course.DoesNotExist:
+        return Response({"message":"Member does not exist"}, status=status.HTTP_404_NOT_FOUND)
+    
+    course.delete()
+
+    return Response({"message":"Member deleted successfully"}, status=status.HTTP_200_OK)
