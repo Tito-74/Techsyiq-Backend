2a7a7c6fe2ef2baf7c5f7b441c0a0362c6bf468e
Tito-74 <kipkirui133@gmail.com>
2023-04-09 13:49:50 +0300

    added course model and serializer

--- Course/models.py
+++ Course/models.py
@@ -2,4 +2,6 @@ from django.db import models
 
 # Create your models here.
 class Course(models.Model):
-    pass
+    name = models.CharField(max_length=200, null=False, blank=False)
+    description = models.TextField(null=False, blank=False)
+    period = models.CharField(max_length=50, null=False, blank=False)
