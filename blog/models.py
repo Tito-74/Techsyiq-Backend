a910a7386a736c6f7fd26c32ee7ae929f0f43446
Tito-74 <kipkirui133@gmail.com>
2023-04-13 12:20:16 +0300

    replace richtext field with textfield

--- blog/models.py
+++ blog/models.py
@@ -13,7 +13,7 @@ class Category(models.Model):
 
 class Blog(models.Model):
   title = models.CharField(max_length=255, blank = False)
-  description = RichTextField(null=False, blank=False)
+  description = models.TextField(null=False, blank=False)
   image = CloudinaryField('images', default=None)
   date_published = models.DateTimeField(auto_now_add=True,verbose_name='date_published')
   author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
