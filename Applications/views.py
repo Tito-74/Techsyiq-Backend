c31c8ac517cf825ab0314914dec171fff92092f4
Tito-74 <kipkirui133@gmail.com>
2023-04-13 21:41:43 +0300

    added function to send mail

--- Applications/views.py
+++ Applications/views.py
@@ -6,16 +6,19 @@ from rest_framework.permissions import AllowAny
 from rest_framework import status
 from .serializers import ApplicationSerializer
 from .models import Application
+from django.core.mail import send_mail
 
 # Create your views here.
 
 @api_view(['POST'])
 @permission_classes([AllowAny])
 @csrf_exempt
-def create_application(request, *args, **kwargs):
+def create_application(request):
     serializer = ApplicationSerializer(data=request.data)
     if serializer.is_valid():
         serializer.save()
+        send_mail('Application Recieve','Your application was recieved and we are evaluating your documents.','kipkirui133@gmail.com',[request.data['email']], fail_silently = True)
+        print("done sending mail")
         return Response(serializer.data, status=status.HTTP_201_CREATED)
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
