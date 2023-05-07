0bc1e6eed106bda0d331c9f0c0ace45580230c81
Tito-74 <kipkirui133@gmail.com>
2023-05-07 21:51:53 +0300

    added  subscribe  func

--- marketing/views.py
+++ marketing/views.py
@@ -49,3 +49,11 @@ def update_figures_details(request, pk):
 
 
 # Subscription
+@api_view(["POST"])
+@csrf_exempt
+def subscribe(request):
+   serializer = SubscriptionSerializer(data = request.data)
+   if serializer.is_valid():
+      serializer.save()
+      return Response(serializer.data, status = status.HTTP_201_CREATED)
+   return Response(serializer.error, status = status.HTTP_400_BAD_REQUEST)
