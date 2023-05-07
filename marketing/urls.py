d37cb6a71331bdc052a93b275eabc782a209f871
Tito-74 <kipkirui133@gmail.com>
2023-05-07 21:51:18 +0300

    added subscribe endpoint

--- marketing/urls.py
+++ marketing/urls.py
@@ -2,7 +2,11 @@ from django.urls import path
 from .views import *
 
 urlpattern = [
+    # figure analysis
     path("/create", create_figure_analysis, name="create"),
     path("/fetch_analysis_details/", fetch_figure_analysis, name="fetch_analysis_details"),
     path("update_analysis_details", update_figures_details, name="update_analysis_details"),
+
+    # subscription
+    path("/subscribe", subscribe, name="subscribe"),
 ]
