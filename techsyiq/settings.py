135f09ce7d92644f85dc5a1d595b8a97903aafe9
Tito-74 <kipkirui133@gmail.com>
2023-05-07 22:46:11 +0300

    updated settings

--- techsyiq/settings.py
+++ techsyiq/settings.py
@@ -11,12 +11,15 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
+# from decouple import config
 import os
 import cloudinary
 import cloudinary.uploader
 import cloudinary.api
 
-
+from dotenv import load_dotenv
+load_dotenv()
 
 cloudinary.config( 
   cloud_name = "dlkjbxanc", 
@@ -119,6 +122,7 @@ DATABASES = {
 }
 
 
+
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
@@ -161,17 +165,12 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST='smtp.gmail.com'
-EMAIL_HOST_USER='lankip.test@gmail.com'
-EMAIL_HOST_PASSWORD='sobqqnopuisxmdvn'
-EMAIL_PORT=587
-EMAIL_USE_TLS=True
-EMAIL_USE_SSL=False
-# EMAIL_HOST = os.environ.get("EMAIL_HOST")
-# EMAIL_HOST_USER = 'lankip.test@gmail.com'
-# EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
-# EMAIL_PORT = os.environ.get('EMAIL_PORT')
-# EMAIL_USE_TLS = os.environ.get('EMAIL_USE_TLS')
-# EMAIL_USE_SSL = os.environ.get('EMAIL_USE_SSL')
+EMAIL_HOST= os.environ.get("EMAIL_HOST")
+EMAIL_HOST_USER= os.environ.get("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD= os.environ.get("EMAIL_HOST_PASSWORD")
+EMAIL_PORT=os.environ.get("EMAIL_PORT")
+EMAIL_USE_TLS=os.environ.get("EMAIL_USE_TLS")
+EMAIL_USE_SSL=os.environ.get("EMAIL_USE_SSL")
 
-print("email", EMAIL_HOST_USER)
+
+# print("email", EMAIL_PORT)
